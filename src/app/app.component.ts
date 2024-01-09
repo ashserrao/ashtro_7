bde646d36b189c78a6eeece1b3ad4f1465a6013d
Anush Serrao <60592571+ashserrao@users.noreply.github.com>
2024-01-09 23:56:33 +0530

    loading_fresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,10 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  // standalone: true, // Remove this line
-  // imports: [CommonModule, RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'strike-pack';
